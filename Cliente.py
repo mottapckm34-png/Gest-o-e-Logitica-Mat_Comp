4a5f077ffc79598ccea495bbe928880e8f312423
Gabriel Motta <motta.pc.km34@gmail.com>
2026-05-19 12:23:41 -0300

    pre almoço

---
+++ Cliente.py
@@ -0,0 +1,17 @@
+ import dataclass
+
+from dataclasses
+class Cliente:
+# id : int
+# nome : str
+latitude : float
+longitude : float
+peso: float
+prazo: float
+
+
+
+
+
+
+
---
+++ Deposito.py
@@ -0,0 +1,9 @@
+
+from dataclasses import dataclass
+
+
+@dataclass
+class Deposito:
+    nome: str
+    latitude: float
+    longitude: float

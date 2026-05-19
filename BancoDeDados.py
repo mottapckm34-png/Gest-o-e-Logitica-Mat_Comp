38c22461ab7ea9c7fb1b2400e6e56331fdbcbd24
Caio Guilherme <cguilherme2005@gmail.com>
2026-05-19 12:39:32 -0300

    Começo da estrutura do armazenamento dos dados

---
+++ BancoDeDados.py
@@ -0,0 +1,27 @@
+import numpy as np
+import pandas as pd
+
+#Inicinado as matrizes de clientes commo listas vazias para serem inseridos os dados na matriz
+ids = []
+latitudes = []
+longitudes = []
+weights = []
+
+#Cadastro do porto central
+print("Cadastro do porto central")
+ids.append(0) 
+latitudes.append(float(input("Latitude do porto: ")))
+
+project_data = { #dados dos clientes
+    'id': ids,
+    'latitude': latitudes, 
+    'longitude': longitudes,
+    'cargo_weight': weights
+}
+
+df_client = pd.DataFrame(project_data) #matriz de cliente principal
+
+df_client.set_index('id', inplace=True) #garante que o iD é o indice da tabela para a busca ser mais facil
+
+print ("Matriz dos clientes")
+print (df_client)

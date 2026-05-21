3a59cd6ec7b6ddd4f32ce644d27b156504740ae2
Gabriel Motta <motta.pc.km34@gmail.com>
2026-05-21 15:39:28 -0300

    modified:   AtributosDoNavio.py

--- AtributosDoNavio.py
+++ AtributosDoNavio.py
@@ -1,11 +1,10 @@
 from dataclasses import dataclass
 
-    @dataclass
+@dataclass
+class AtributosDoNavio:
 
-    class AtributosDoNavio:
-
-        velocidadeMediaDoNavio: float # em milhas nauticas por hora
-        cargaMaximaDoNavio: float # em toneladas, ou seja, 1000 kg
-        combustivelPorMilhaNautica: float # em litros por milha nautica, ou seja. 0.5 litros por milha nautica
-        custoPorLitroDeCombustivel: float # em reais por litro, ou seja, 5 reais por litro
-        custoFixoPorViagem: float # em reais, ou seja, 1000
+    velocidadeMediaDoNavio: float # em milhas nauticas por hora
+    cargaMaximaDoNavio: float # em toneladas, ou seja, 1000 kg
+    combustivelPorMilhaNautica: float # em litros por milha nautica, ou seja. 0.5 litros por milha nautica
+    custoPorLitroDeCombustivel: float # em reais por litro, ou seja, 5 reais por litro
+    custoFixoPorViagem: float # em reais, ou seja, 1000
--- BancoDeDados.py
+++ BancoDeDados.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas as pd
 
 #Inicinado as matrizes de clientes commo listas vazias para serem inseridos os dados na matriz
@@ -7,39 +8,21 @@ longitudes = []
 weights = []
 
 #Cadastro do porto central
-print("Cadastro do porto central\n")
-ids.append(0) 
+print("Cadastro do porto central")
+id.append(0) 
 latitudes.append(float(input("Latitude do porto: ")))
-longitudes.append(float(input("Longitude do porto: ")))
-weights.append(0.0)
 
-#Cadastro dos clientes
-print("\nCadastro dos clientes\n")
-qtd_clientes = int(input("Digite a quatidade de clientes que deseja cadastrar: ")) #digitar a quantidade de clientes  que deseja acrescentar
-
-for i  in range(1,qtd_clientes + 1): #Percorre a quantidade de clientes
-    print(f"\nDados do cliente {i}:")
-    ids.append(i) #adiciona um ID no final da  lista
-
-
-#insere manualmente os dados dos clientes para fazer teste de funcionamento, depois iremos remover
-    latitudes.append(float(input(f"Latitude do cliente {i}: ")))
-    longitudes.append(float(input(f"Longitude do cliente {i}: ")))
-
-    weights.append(float(input(f"Peso da carga do cliente {i} (toneladas): "))) #tonaeladas por ser barcos
-
-
-#dados dos clientes
-project_data = {
-    'id': ids,
+project_data = { #dados dos clientes
+    'id': id,
     'latitude': latitudes, 
     'longitude': longitudes,
     'cargo_weight': weights
 }
 
-df_client = pd.DataFrame(project_data) #matriz de clientes
+df_client = pd.DataFrame(project_data) #matriz de cliente principal
 
 df_client.set_index('id', inplace=True) #garante que o iD é o indice da tabela para a busca ser mais facil
 
-print ("Matriz de armazenamento de dados gerada")
-print (df_client) #printa a matriz de clientes para a conferencia dos dados
+print ("Matriz dos clientes")
+print (df_client)
+

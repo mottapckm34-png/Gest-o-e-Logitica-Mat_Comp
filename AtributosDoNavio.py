26467c050dfe39660f905d8e7267e4fa8b0298e9
Gabriel Motta <motta.pc.km34@gmail.com>
2026-05-21 15:36:54 -0300

    modified:   AtributosDoNavio.py modified:   BancoDeDados.py (talvez seja necessário adicionar os dados dos clientes, como id, latitude, longitude, peso, janela de tempo e tempo de serviço)

--- AtributosDoNavio.py
+++ AtributosDoNavio.py
@@ -1,10 +1,11 @@
 from dataclasses import dataclass
 
-@dataclass
-class AtributosDoNavio:
+    @dataclass
 
-VelocidadeMediaDoNavio: float # em milhas nauticas por hora
-CargaMaximaDoNavio: float # em toneladas, ou seja, 1000 kg
-CombustivelPorMilhaNautica: float # em litros por milha nautica, ou seja. 0.5 litros por milha nautica
-CustoPorLitroDeCombustivel: float # em reais por litro, ou seja, 5 reais por litro
-CustoFixoPorViagem: float # em reais, ou seja, 1000
+    class AtributosDoNavio:
+
+        velocidadeMediaDoNavio: float # em milhas nauticas por hora
+        cargaMaximaDoNavio: float # em toneladas, ou seja, 1000 kg
+        combustivelPorMilhaNautica: float # em litros por milha nautica, ou seja. 0.5 litros por milha nautica
+        custoPorLitroDeCombustivel: float # em reais por litro, ou seja, 5 reais por litro
+        custoFixoPorViagem: float # em reais, ou seja, 1000
